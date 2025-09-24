56ff88ff13a45b19a222e926113ab10cf1bf7e4d
Geison <geisonshorts@gmail.com>
2025-09-24 20:06:10 -0300

    Adicionado beforeEach para acessar a pagina web

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,23 +1,27 @@
 describe('login', () => {
-  it('Login com dados validos deve permitir efetuar login', () => {
+
+  beforeEach(() => {
     cy.visit('http://localhost:4000');
+  })
+
+
+  it('Login com dados validos deve permitir efetuar login', () => {
 
     cy.get('#username').click().type('julio.lima');
     cy.get('#senha').click().type("123456");
     cy.contains('button', 'Entrar').click();
 
     cy.contains('h4', 'Realizar Transferência').should('be.visible');
-    // abre o Google
+ 
   });
 
   it('login com dados invalidos deve apresentar erro', () => {
-    cy.visit('http://localhost:4000');
 
     cy.get('#username').click().type('julio.lima');
     cy.get('#senha').click().type("12345");
     cy.contains('button', 'Entrar').click();
 
     cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.');
-    // abre o Google
+   
   });
 });
