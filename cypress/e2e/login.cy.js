f3615274e9f15724a8a2c891cf90b0af146aac54
Geison <geisonshorts@gmail.com>
2025-09-28 13:43:03 -0300

    Adicionado beforeEach ao projeto

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,10 @@
 describe('Login', () => {
-  it('Login com dados validos deve permitir acesso ao site do banco web', () => {
+
+  beforeEach(() => {
     cy.visit('http://localhost:4000')
+  });
+  
+  it('Login com dados validos deve permitir acesso ao site do banco web', () => {
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
     cy.contains('button', 'Entrar').click()
@@ -8,8 +12,7 @@ describe('Login', () => {
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
   }),
 
-    it.only('Login com dados invalidos não deve permitir acesso ao site do banco web', () => {
-      cy.visit('http://localhost:4000')
+    it('Login com dados invalidos não deve permitir acesso ao site do banco web', () => {
       cy.get('#username').click().type('julio.lima')
       cy.get('#senha').click().type('12345')
       cy.contains('button', 'Entrar').click()
