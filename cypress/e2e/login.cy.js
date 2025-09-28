854a0de49e969e802a10287994a77e1c0b9a95bd
Geison <geisonshorts@gmail.com>
2025-09-28 13:34:30 -0300

    Adicionado teste com login inválido

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,8 +3,17 @@ describe('Login', () => {
     cy.visit('http://localhost:4000')
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
-    cy.contains('button','Entrar').click()
+    cy.contains('button', 'Entrar').click()
 
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
-  })
+  }),
+
+    it.only('Login com dados invalidos não deve permitir acesso ao site do banco web', () => {
+      cy.visit('http://localhost:4000')
+      cy.get('#username').click().type('julio.lima')
+      cy.get('#senha').click().type('12345')
+      cy.contains('button', 'Entrar').click()
+
+      cy.contains('.toast', 'Erro no login. Tente novamente').should('be.visible')
+    })
 })
